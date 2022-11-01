5c7b67f214726479a0b2dbc735f4da79effcc8a8
acaipal <acaipal@posgrado.upv.es>
2022-11-01 02:36:19 +0100

    Credenciales BD Heroku

--- src/connections/database.js
+++ src/connections/database.js
@@ -2,14 +2,14 @@ import mysql from 'promise-mysql'
 import config from './../configuration'
 
 const connection = mysql.createConnection({
-  host: config.host,
+  /* host: config.host,
   database: config.database,
   user: config.user,
-  password: config.password
-  /* host: 'eu-cdbr-west-03.cleardb.net',
+  password: config.password */
+  host: 'eu-cdbr-west-03.cleardb.net',
   database: 'heroku_4b6f5443ffb8ab5',
   user: 'bd0dc601319000',
-  password: 'd5fce2cf' */
+  password: 'd5fce2cf'
 })
 
 const getConnectionDb = () => {
