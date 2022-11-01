d32bfa74538294b655542041b5664a920a3331dc
acaipal <acaipal@posgrado.upv.es>
2022-11-01 05:09:53 +0100

    conexionbd

--- src/connections/database.js
+++ src/connections/database.js
@@ -1,19 +1,29 @@
 import mysql from 'promise-mysql'
-import config from './../configuration'
+// import config from './../configuration'
 
-const connection = mysql.createConnection({
-  /* host: config.host,
-  database: config.database,
-  user: config.user,
-  password: config.password */
+// const mysqll = require('mysql')
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: 'eu-cdbr-west-03.cleardb.net',
   database: 'heroku_4b6f5443ffb8ab5',
   user: 'bd0dc601319000',
   password: 'd5fce2cf'
 })
 
+/* host: config.host,
+  database: config.database,
+  user: config.user,
+  password: config.password */
+
+/* const connection = mysql.createConnection({
+  host: 'eu-cdbr-west-03.cleardb.net',
+  database: 'heroku_4b6f5443ffb8ab5',
+  user: 'bd0dc601319000',
+  password: 'd5fce2cf'
+}) */
+
 const getConnectionDb = () => {
-  return connection
+  return pool
 }
 
 export default getConnectionDb
